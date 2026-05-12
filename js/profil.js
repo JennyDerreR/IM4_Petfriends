b3fb85f987a67bb6f8704b42d821cf624f696d5b
Jennifer Derrer <jennifer.derrer@stud.fhgr.ch>
2026-05-12 11:26:32 +0200

    remodelling Struktur + Start Profil Email und Passowrtreset

--- js/profil.js
+++ js/profil.js
@@ -8,6 +8,8 @@ async function loadProfile () {
     const result = await response.json();
     console.log ("Profile data:", result);
 
+    document.querySelector("#familienname").value = result.familienname || "";
+    document.querySelector("#email").value = result.email || "";
 
   } catch (error) {
     console.error("Error:", error);
@@ -15,17 +17,13 @@ async function loadProfile () {
   }
 }
 
-
-
 loadProfile();
 
-
 document.getElementById("profilForm")
-  addEventListener("submit", async (e) => {
+  .addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const familienname = document.getElementById("familienname").value.trim();
-
 
     try {
         const response = await fetch("api/profilUpdate.php", {
@@ -38,14 +36,64 @@ document.getElementById("profilForm")
       const result = await response.json();
       console.log("Update response:", result);
 
+      if (result.status === "error") { 
+        window.location.href = "login.html";
+        return;
+    }
+
+    loadProfile(); // Refresh profile data after update
+
      /* if (result.status === "success") {
         alert("Registration successful! You can now log in.");
         window.location.href = "login.html";
       } else {
         alert(result.message || "Registration failed.");
       }*/
+
     } catch (error) {
-      console.error("Error:", error);
-      alert("Something went wrong!");
-    }
+  console.error("Error:", error);
+
+  alert(error.message);
+}
   });
+
+document
+  .getElementById("passwortAendern")
+  .addEventListener("click", async () => {
+
+    try {
+
+      const response = await fetch(
+        "api/passwortReset.php",
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+
+      const result = await response.json();
+
+      if (result.status === "success") {
+
+        alert(
+          "Eine Email zum Zurücksetzen wurde gesendet."
+        );
+
+      } else {
+
+        alert(
+          result.message || "Fehler."
+        );
+
+      }
+
+    } catch (error) {
+
+      console.error(error);
+
+      alert(error.message);
+
+    }
+
+});
+
